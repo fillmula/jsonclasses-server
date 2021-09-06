c1e6799da925d1a3a0678539c13195066fcee051
Victor Teo <victor.teo@fillmula.com>
2021-09-06 13:32:11 +0800

    setup project

---
+++ setup.py
@@ -0,0 +1,23 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='jsonclasses-server',
+      version='2.7.0',
+      description='jsonclasses server',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      author='Fillmula Inc.',
+      author_email='victor.teo@fillmula.com',
+      license='MIT',
+      packages=find_packages(exclude=("tests")),
+      package_data={'jsonclasses_server': ['py.typed']},
+      zip_safe=False,
+      url='https://github.com/fillmula/jsonclasses-server',
+      include_package_data=True,
+      python_requires='>=3.9')
