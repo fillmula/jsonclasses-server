fa450b6b3c707c2acb8b6fdaf6547c97acd1ba26
Victor Teo <victor.teo@fillmula.com>
2021-09-07 09:26:57 +0800

    record

--- jsonclasses_server/api_class.py
+++ jsonclasses_server/api_class.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import ClassVar
+from jsonclasses.jobject import JObject
 from .aconf import AConf
 from .nameutils import (
     cname_to_pname, fname_to_pname, pname_to_cname, pname_to_fname
@@ -28,12 +29,20 @@ class API:
             fname_to_pname=fname_to_pname,
             pname_to_cname=pname_to_cname,
             pname_to_fname=pname_to_fname)
+        self._starting_names: dict[str, type[JObject]] = {}
         self.__class__._initialized_map[graph_name] = True
         return None
 
     @property
     def aconf(self: API) -> AConf:
+        """The default API configuration for all classes on this graph."""
         return self._default_aconf
+
+    def record(self: API, cls: type[JObject], aconf: AConf) -> None:
+        name = aconf.cname_to_pname(cls.__name__)
+        self._starting_names[name] = cls
+
+        pass
 
     default: ClassVar[API]
 
