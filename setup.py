8d030a592be9622fa2d5f8400dd4e8449b7aafcd
Ben Wong <ben.wong@fillmula.com>
2021-10-28 15:19:46 +0800

    change version 3.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='jsonclasses-server',
-      version='3.0.0',
+      version='3.0.1',
       description='jsonclasses server',
       long_description=README,
       long_description_content_type="text/markdown",
